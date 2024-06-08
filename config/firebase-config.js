be47ed2f19ba8b7743e159b6399d49b5c5427d7e
Ang-Wei-Liang <113972542+Ang-Wei-Liang@users.noreply.github.com>
2024-06-09 03:44:42 +0800

    Update firebase-config.js

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -11,13 +11,7 @@
 firebase.initializeApp(firebaseConfig);*/
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAQANNYfNIn90563GO7Z5VP0ajOwJ_C2_0",
-    authDomain: "roastme-c7654.firebaseapp.com",
-    projectId: "roastme-c7654",
-    storageBucket: "roastme-c7654.appspot.com",
-    messagingSenderId: "841573246670",
-    appId: "1:841573246670:web:9ba6c3e53ab99fd331529f",
-    measurementId: "G-W9DSPKVQ0T" // Note: Added measurementId
+    
   };
   
   
